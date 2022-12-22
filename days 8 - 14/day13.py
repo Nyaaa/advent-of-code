a300fd5ace84022ee90c4a70997448f30eb1a9a5
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-12-23 00:17:48 +0300

    day13 part 2

--- days 8 - 14/day13.py
+++ days 8 - 14/day13.py
@@ -1,17 +1,13 @@
-from tools import parsers
 from itertools import zip_longest
+from tools import parsers
 
-data = parsers.blocks('input13.txt')
 
-
-def compare(left, right):
+def compare(left, right) -> bool:
     if not isinstance(left, list): left = [left]
     if not isinstance(right, list): right = [right]
 
     zipped = list(zip_longest(left, right))
-    # print(zipped)
     for i, j in zipped:
-        # print(i, j)
         if j is None:
             return False
         elif i is None:
@@ -26,13 +22,30 @@ def compare(left, right):
                 return out
 
 
+def flatten(list_of_lists: list) -> list:
+    if len(list_of_lists) == 0:
+        return list_of_lists
+    if isinstance(list_of_lists[0], list):
+        if len(list_of_lists[0]) == 0:
+            list_of_lists[0] = [0]
+        return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
+    return list_of_lists[:1] + flatten(list_of_lists[1:])
+
+
+# part 1
+data = parsers.blocks('input13.txt')
 result = {True: [], False: []}
 for index in range(1, len(data) + 1):
     c_left, c_right = data[index - 1]
     c_left, c_right = eval(c_left), eval(c_right)
     res = compare(c_left, c_right)
     result[res].append(index)
-    # print('index', index, res)
-
-print(result)
 print(sum(result[True]))  # 5252
+
+# part 2
+data = parsers.lines('input13.txt')
+data.append('[[2]]')
+data.append('[[6]]')
+sort = [flatten(eval(line)) for line in data if line != '']
+sort.sort()
+print((sort.index([2]) + 1) * (sort.index([6]) + 1))  # 20592
