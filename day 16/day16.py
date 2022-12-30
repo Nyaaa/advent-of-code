1df721852b8cae7bfb2eb13c7614d25a0af6edb9
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2022-12-31 00:32:26 +0300

    day 16

--- day 16/day16.py
+++ day 16/day16.py
@@ -1,7 +1,6 @@
 from tools import parsers
 import networkx as nx
 import re
-import operator
 
 
 class Cave:
@@ -14,6 +13,7 @@ class Cave:
         [('AA', 'DD'), ('AA', 'II'), ('AA', 'BB')]
         """
         self.cavern = nx.Graph()
+        self.path = ['AA']
 
         for line in data:
             res = re.findall(r"[A-Z]+[A-Z]|\d+", line)
@@ -22,20 +22,51 @@ class Cave:
             for i in conn:
                 self.cavern.add_edge(node, i, weight=1)
 
-    def part_1(self):
-        source = 'AA'
-        # flow = nx.get_node_attributes(self.cavern, 'flowrate')
-        # top_flow = dict(sorted(flow.items(), key=lambda item: item[1], reverse=True))
-        # print(top_flow)
-        timer = 30
-        value = 0
+    def evaluate_nodes(self, source: str, timer: int):
+        time_left = 30 - timer
+        values = []
+        target = (-99, 'AA', 0, 0)
         for node in self.cavern.nodes:
-            if not self.cavern.nodes[node]['open']:
-                print(node)
+            flow = self.cavern.nodes[node]['flowrate']
+            if not self.cavern.nodes[node]['open'] and flow > 0:
                 dist = nx.shortest_path_length(self.cavern, source=source, target=node)
-                print(dist)
+                value = (flow - dist) * time_left
+                # recursion?
+
+                values.append((value, node, dist, flow))
+        print(values)
+        while values:
+            candidate = values.pop()
+            if (candidate[0] > target[0]) or (candidate[0] == target[0] and candidate[2] < target[2]):
+                target = candidate
+        print(f'target: {target[1]}')
+        return target
+
+    def part_1(self):
+        """test part 1:
+
+        1651"""
+        timer = 0
+        flow_per_minute = 0
+        total_flow = 0
+        # print(nx.floyd_warshall(self.cavern))
+        while timer < 30:
+            timer += 1
+            print(f"{'=' * 10} {timer}")
+            total_flow += flow_per_minute
+            current = self.path[-1]
+            _, target, dist, flow = self.evaluate_nodes(current, timer)
+            if current != target:
+                path = nx.shortest_path(self.cavern, source=current, target=target)
+                self.path.append(path[1])
+                print(f'{current} -> {target}')
+            else:
+                flow_per_minute += flow
+                self.cavern.nodes[target]['open'] = True
+                print(f'opening {target}')
+            print(f'fpm: {flow_per_minute}')
+        return total_flow
 
 
-c = Cave(parsers.lines('test.txt')).part_1()
-
-
+print(Cave(parsers.lines('test.txt')).part_1())
+# print(Cave(parsers.lines('input.txt')).part_1())  # 1310 too low
