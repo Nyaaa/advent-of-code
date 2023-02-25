94199da2a34628b081e0f90aa21bbca360588da9
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-02-25 16:10:13 +0300

    day 20 part 2

--- 2022/day 20/day20.py
+++ 2022/day 20/day20.py
@@ -6,8 +6,8 @@ test = ['1', '2', '-3', '3', '-2', '0', '4']
 
 class Decrypt:
     def __init__(self, data):
-        ints = [int(line) for line in data]
-        self.code: deque = deque(enumerate(ints))  # (index, value)
+        self.ints = [int(line) for line in data]
+        self.code: deque = deque(enumerate(self.ints))  # (index, value)
 
     def mix(self):
         for i in range(len(self.code)):
@@ -21,18 +21,29 @@ class Decrypt:
     def count(self):
         values = []
         deenum = [i[1] for i in self.code]
-        for i in (1000, 2000, 3000):
+        for i in 1000, 2000, 3000:
             pos = (deenum.index(0) + i) % len(deenum)
             values.append(deenum[pos])
-        return values
+        return sum(values)
 
     def part_1(self):
         """test part 1:
         >>> print(Decrypt(test).part_1())
         3"""
         self.mix()
-        values = self.count()
-        return sum(values)
+        return self.count()
+
+    def part_2(self):
+        """test part 1:
+        >>> print(Decrypt(test).part_2())
+        1623178306"""
+        key = 811589153
+        self.ints = [i * key for i in self.ints]
+        self.code = deque(enumerate(self.ints))
+        for _ in range(10):
+            self.mix()
+        return self.count()
 
 
 print(Decrypt(parsers.lines(loader.get())).part_1())  # 3473
+print(Decrypt(parsers.lines(loader.get())).part_2())  # 7496649006261
