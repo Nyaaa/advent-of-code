1bd5fdf1aaf00eb8869b3bdbf4e34066b78fa74f
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-03-16 11:58:43 +0300

    day 22 part 1

--- 2022/day 22/day22.py
+++ 2022/day 22/day22.py
@@ -25,20 +25,18 @@ class Cube:
     def warp(self, row: int, col: int):
         match self.direction:
             case 'right':
-                left = np.nonzero(self.map[row])[0][0]
-                return self.point(left, row)
+                col = np.nonzero(self.map[row])[0][0]
             case 'left':
-                right = np.nonzero(self.map[row])[0][-1]
-                return self.point(right, row)
+                col = np.nonzero(self.map[row])[0][-1]
             case 'up':
-                down = np.nonzero(self.map[:, col])[0][-1]
-                return self.point(col, down)
+                row = np.nonzero(self.map[:, col])[0][-1]
             case 'down':
-                up = np.nonzero(self.map[:, col])[0][0]
-                return self.point(col, up)
+                row = np.nonzero(self.map[:, col])[0][0]
+
+        return self.point(col, row)
 
     def move(self, distance: int):
-        print(distance, self.direction)
+        # print(distance, self.direction)
 
         for _ in range(distance):
             col, row = self.location
@@ -46,33 +44,45 @@ class Cube:
 
             match self.direction:
                 case 'right':
-                    _next = self.point(col + 1, row)
+                    col += 1
                 case 'left':
-                    _next = self.point(col - 1, row)
+                    col -= 1
                 case 'up':
-                    _next = self.point(col, row - 1)
+                    row -= 1
                 case 'down':
-                    _next = self.point(col, row + 1)
+                    row += 1
 
             try:
-                cell = self.map[_next[1]][_next[0]]
+                cell = self.map[row][col]
             except IndexError:
                 cell = ' '
 
             if cell == ' ':
                 _next = self.warp(row, col)
-                cell = self.map[_next[1]][_next[0]]
+            else:
+                _next = self.point(col, row)
+            cell = self.map[_next.Row][_next.Column]
 
-            if cell == '.':
+            if cell == '.' or cell == 'X':
                 self.location = _next
             elif cell == '#':
                 self.location = current
 
-            self.map[row][col] = 'X'
+            # self.map[row][col] = 'X'
 
-        print(self.location, self.direction)
+        # print(self.location, self.direction)
+        # print(self.map[row-2:row+2, col-2:col+2])
 
     def part_1(self):
+        """test part 1:
+        >>> c = Cube(parsers.blocks('test.txt'))
+        >>> print(c.part_1())
+        6032
+        >>> c = Cube(parsers.blocks('test.txt'))
+        >>> c.path = parsers.generator([1, 'L', 1, 'L', 1, 'L', 1, 'L'])
+        >>> print(c.part_1())
+        1036
+        """
         while True:
             try:
                 move = int(next(self.path))
@@ -81,18 +91,15 @@ class Cube:
                 turn = 1 if turn == 'R' else 0
                 self.direction = self.DIRECTIONS[self.direction][turn]
             except StopIteration:
-                for i in range(len(self.map)):
-                    print(self.map[i])
-                col = self.location[0] + 1
-                row = self.location[1] + 1
+                # for i in range(len(self.map)):
+                #     print(self.map[i])
+                col = self.location.Column + 1
+                row = self.location.Row + 1
                 direction = self.DIRECTIONS[self.direction][2]
-                print(self.location)
-                print(row, col, direction)
+                # print(self.location, direction)
                 return (1000 * row) + (4 * col) + direction
 
 
-print(Cube(parsers.blocks('test.txt')).part_1())
-print(Cube(parsers.blocks(loader.get())).part_1())
+print(Cube(parsers.blocks(loader.get())).part_1())  # 76332
 
-# 111144 too high
-# Location(Column=35, Row=110)
+# print(Cube(parsers.blocks('test.txt')).part_1())
