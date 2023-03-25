7e96efd8dcbce4b251c941feb9508347de00ade7
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-03-25 15:26:18 +0300

    day 22 part 2

--- 2022/day 22/day22.py
+++ 2022/day 22/day22.py
@@ -1,86 +1,127 @@
 from tools import parsers, loader
 import re
-from collections import namedtuple
+from typing import NamedTuple
 import numpy as np
 
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+DIRECTIONS = {'up': ('left', 'right', 3),
+              'right': ('up', 'down', 0),
+              'down': ('right', 'left', 1),
+              'left': ('down', 'up', 2)}
+
+
+class Point(NamedTuple):
+    col: int
+    row: int
 
 
 class Cube:
-    DIRECTIONS = {'up': ('left', 'right', 3),
-                  'right': ('up', 'down', 0),
-                  'down': ('right', 'left', 1),
-                  'left': ('down', 'up', 2)}
-
-    def __init__(self, data):
+    def __init__(self, data, part2=False):
+        self.part2 = part2
         _map = [list(i) for i in data[0]]
         path = re.split(r"(\d+)([A-Z])", *data[1])
         self.path = parsers.generator(list(filter(None, path)))
-        self.point = namedtuple('Location', 'Column, Row')
         self.direction = 'right'
         max_len = np.max([len(a) for a in _map])
         self.map = np.asarray([np.pad(a, (0, max_len - len(a)), 'constant', constant_values=' ') for a in _map])
-        self.location = self.point(np.nonzero(self.map[0] == '.')[0][0], 0)
+        self.location = Point(np.nonzero(self.map[0] == '.')[0][0], 0)
 
-    def warp(self, row: int, col: int):
+    def warp(self, row: int, col: int) -> Point:
         match self.direction:
-            case 'right':
-                col = np.nonzero(self.map[row])[0][0]
-            case 'left':
-                col = np.nonzero(self.map[row])[0][-1]
-            case 'up':
-                row = np.nonzero(self.map[:, col])[0][-1]
-            case 'down':
-                row = np.nonzero(self.map[:, col])[0][0]
+            case 'right': col = np.nonzero(self.map[row])[0][0]
+            case 'left': col = np.nonzero(self.map[row])[0][-1]
+            case 'up': row = np.nonzero(self.map[:, col])[0][-1]
+            case 'down': row = np.nonzero(self.map[:, col])[0][0]
+        return Point(col, row)
 
-        return self.point(col, row)
+    def draw(self):
+        for i in range(len(self.map)):
+            print(self.map[i])
+
+    def shift(self, row: int, col: int) -> tuple[Point, str]:
+        """Hardcoded rotations because screw this puzzle.
+
+            |1|2|
+            |3|
+          |5|4|
+          |6|
+
+        """
+        if 0 <= row <= 49 and 50 <= col <= 99: face = 1
+        elif 0 <= row <= 49 and 100 <= col <= 149: face = 2
+        elif 50 <= row <= 99 and 50 <= col <= 99: face = 3
+        elif 100 <= row <= 149 and 50 <= col <= 99: face = 4
+        elif 100 <= row <= 149 and 0 <= col <= 49: face = 5
+        elif 150 <= row <= 199 and 0 <= col <= 49: face = 6
+        else: raise IndexError('Point is not on any of the faces.')
+
+        # positions relative to face
+        rel_row, rel_col = row % 50, col % 50
+
+        match (face, self.direction):
+            case(1, 'left'): mv = Point(0, 149 - rel_row), 'right'  # -> face 5
+            case(1, 'up'): mv = Point(0, rel_col + 150), 'right'  # -> face 6
+
+            case (2, 'up'): mv = Point(rel_col, 199), 'up'  # -> face 6
+            case (2, 'right'): mv = Point(99, 149 - rel_row), 'left'  # -> face 4
+            case (2, 'down'): mv = Point(99, rel_col + 50), 'left'  # -> face 3
+
+            case (3, 'left'): mv = Point(rel_row, 100), 'down'  # -> face 5
+            case (3, 'right'): mv = Point(rel_row + 100, 49), 'up'  # -> face 2
+
+            case (4, 'down'): mv = Point(49, rel_col + 150), 'left'  # -> face 6
+            case (4, 'right'): mv = Point(149, 49 - rel_row), 'left'  # -> face 2
+
+            case (5, 'left'): mv = Point(50, 49 - rel_row), 'right'  # -> face 1
+            case (5, 'up'): mv = Point(50, rel_col + 50), 'right'  # -> face 3
+
+            case (6, 'right'): mv = Point(rel_row + 50, 149), 'up'  # -> face 4
+            case (6, 'down'): mv = Point(rel_col + 100, 0), 'down'  # -> face 2
+            case (6, 'left'): mv = Point(rel_row + 50, 0), 'down'  # -> face 1
+
+            case _:
+                self.draw()
+                raise NotImplementedError(f'No transition {self.direction} from face {face}.')
+        return mv
 
     def move(self, distance: int):
         # print(distance, self.direction)
 
         for _ in range(distance):
             col, row = self.location
-            current = self.point(col, row)
+            current_location = self.location
+            current_direction = self.direction
 
             match self.direction:
-                case 'right':
-                    col += 1
-                case 'left':
-                    col -= 1
-                case 'up':
-                    row -= 1
-                case 'down':
-                    row += 1
+                case 'right': col += 1
+                case 'left': col -= 1
+                case 'up': row -= 1
+                case 'down': row += 1
 
             try:
                 cell = self.map[row][col]
             except IndexError:
                 cell = ' '
 
-            if cell == ' ':
-                _next = self.warp(row, col)
-            else:
-                _next = self.point(col, row)
-            cell = self.map[_next.Row][_next.Column]
+            match (cell, self.part2):
+                case (' ', False): _next = self.warp(row, col)
+                case (' ', True): _next, self.direction = self.shift(current_location.row, current_location.col)
+                case _: _next = Point(col, row)
 
-            if cell == '.' or cell == 'X':
-                self.location = _next
-            elif cell == '#':
-                self.location = current
+            match self.map[_next.row][_next.col]:
+                case '.' | 'X': self.location = _next
+                case '#': self.location, self.direction = current_location, current_direction
 
-            # self.map[row][col] = 'X'
+            self.map[self.location.row][self.location.col] = 'X'
 
-        # print(self.location, self.direction)
-        # print(self.map[row-2:row+2, col-2:col+2])
-
-    def part_1(self):
+    def start(self):
         """test part 1:
         >>> c = Cube(parsers.blocks('test.txt'))
-        >>> print(c.part_1())
+        >>> print(c.start())
         6032
         >>> c = Cube(parsers.blocks('test.txt'))
         >>> c.path = parsers.generator([1, 'L', 1, 'L', 1, 'L', 1, 'L'])
-        >>> print(c.part_1())
+        >>> print(c.start())
         1036
         """
         while True:
@@ -89,17 +130,14 @@ class Cube:
                 self.move(move)
                 turn = next(self.path)
                 turn = 1 if turn == 'R' else 0
-                self.direction = self.DIRECTIONS[self.direction][turn]
+                self.direction = DIRECTIONS[self.direction][turn]
             except StopIteration:
-                # for i in range(len(self.map)):
-                #     print(self.map[i])
-                col = self.location.Column + 1
-                row = self.location.Row + 1
-                direction = self.DIRECTIONS[self.direction][2]
-                # print(self.location, direction)
+                col = self.location.col + 1
+                row = self.location.row + 1
+                direction = DIRECTIONS[self.direction][2]
+                # self.draw()
                 return (1000 * row) + (4 * col) + direction
 
 
-print(Cube(parsers.blocks(loader.get())).part_1())  # 76332
-
-# print(Cube(parsers.blocks('test.txt')).part_1())
+print(Cube(parsers.blocks(loader.get())).start())  # 76332
+print(Cube(parsers.blocks(loader.get()), part2=True).start())  # 144012
