a58cf8bd626d01f1f5c8daee9c7e5496f7affd9e
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-04-28 00:50:53 +0300

    day 12

--- 2021/day 12/day12.py
+++ 2021/day 12/day12.py
@@ -10,8 +10,24 @@ class Graph:
             self.nodes[name].append(conn)
             self.nodes[conn].append(name)
 
-    def part_1(self):
-        return self.nodes
+    def count_paths(self, extra_visit: bool, node: str = 'start', seen: tuple = ('start', )) -> int:
+        """
+        >>> print(Graph(parsers.lines('test.txt')).count_paths(extra_visit=False))
+        10
+
+        >>> print(Graph(parsers.lines('test.txt')).count_paths(extra_visit=True))
+        36"""
+        if node == 'end':
+            return 1
+        paths = 0
+
+        for conn in self.nodes[node]:
+            if not conn.islower() or conn not in seen:
+                paths += self.count_paths(extra_visit, conn, seen + (conn, ))
+            elif extra_visit and conn != 'start':
+                paths += self.count_paths(False, conn, seen + (conn, ))
+        return paths
 
 
-print(Graph(parsers.lines('test.txt')).part_1())
+print(Graph(parsers.lines(loader.get())).count_paths(extra_visit=False))  # 4413
+print(Graph(parsers.lines(loader.get())).count_paths(extra_visit=True))  # 118803
