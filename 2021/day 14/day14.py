044c860cab5defdc4b9306bd893d23e500a232ce
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-05-01 20:00:21 +0300

    day 14 part 2

--- 2021/day 14/day14.py
+++ 2021/day 14/day14.py
@@ -1,35 +1,40 @@
 from collections import Counter
 from tools import parsers, loader
+from itertools import pairwise
+from functools import cache
 
 
 class Polymer:
     def __init__(self, data):
         self.template: str = data[0][0]
         self.rules: dict = {i: j for i, j in (x.split(' -> ') for x in data[1])}
+        self.counter: Counter = Counter(self.template)
 
-    def insert(self, init_line: str):
-        line = ''
-        for i in range(len(init_line)):
-            try:
-                pair = init_line[i:i + 2]
-                if len(pair) == 1:
-                    line += pair
-                    return line
-                line += init_line[i] + self.rules[pair]
-            except IndexError:
-                return line
+    @cache
+    def count(self, pair: str, steps: int):
+        if steps == 0:
+            return None
+        new = self.rules[pair]
+        _counter = Counter(new)
+        for i in pair[0] + new, new + pair[1]:
+            _counter.update(self.count(pair=i, steps=steps - 1))
+        return _counter
 
-    def part_1(self):
-        """
-        >>> print(Polymer(parsers.blocks('test.txt')).part_1())
-        1588"""
-        step = 0
-        while step != 10:
-            self.template = self.insert(self.template)
-            step += 1
-        common = sorted(list(Counter(self.template).values()))
-        return common[-1] - common[0]
+    def start(self, steps: int):
+        """test part 1:
+        >>> print(Polymer(parsers.blocks('test.txt')).start(10))
+        1588
+
+        test part 2:
+        >>> print(Polymer(parsers.blocks('test.txt')).start(40))
+        2188189693529"""
+        for pair in pairwise(self.template):
+            pair = ''.join(pair)
+            self.counter.update(self.count(pair=pair, steps=steps))
+        common = self.counter.most_common()
+        return common[0][1] - common[-1][1]
 
 
-print(Polymer(parsers.blocks(loader.get())).part_1())  # 2223
+print(Polymer(parsers.blocks(loader.get())).start(10))  # 2223
+print(Polymer(parsers.blocks(loader.get())).start(40))  # 2566282754493
 
