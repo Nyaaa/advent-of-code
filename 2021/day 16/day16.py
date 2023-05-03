fe5946e5c039d7f2580f1dfa41d7c0007c666d5e
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-05-03 16:06:21 +0300

    T_T

--- 2021/day 16/day16.py
+++ 2021/day 16/day16.py
@@ -1,10 +1,9 @@
+import math
 from collections import deque
 from dataclasses import dataclass
 from typing import Optional
-import logging
 from tools import parsers, loader
 
-logging.basicConfig(level=logging.DEBUG)
 HEX_TO_BIN = {
     '0': '0000',
     '1': '0001',
@@ -37,27 +36,52 @@ class Decoder:
     def __init__(self, data):
         self.encoded = ''.join(HEX_TO_BIN[i] for i in data[0])
         self.packets = []
-        self.children = []
+        self.root: Optional[Packet]
+        self.parent = deque([])
+        self.part2: bool = False
+
+    def get_type(self, packet: str) -> int:
+        return int(packet[3:6], 2)
 
     def decode_packet(self, packet: str):
         pak_version = int(packet[:3], 2)
         pak_type = int(packet[3:6], 2)
+        pk = Packet(version=pak_version, type=pak_type, children=[])
+        if not self.packets:
+            self.root = pk
+        self.packets.append(pk)
         if pak_type == 4:
             decoded, remainder = self.decode_literal(packet[6:])
-            bb = Packet(version=pak_version, type=pak_type, value=decoded)
-            self.packets.append(bb)
-            self.children.append(decoded)
+            pk.value = decoded
+            self.parent[-1].children.append(decoded)
             if remainder:
+                following = self.get_type(remainder)
+                if following != 4:
+                    self.parent.pop()
                 self.decode_packet(remainder)
-            return self.children
         else:
-            parent = Packet(version=pak_version, type=pak_type)
-            child = self.decode_operator(packet)
-            parent.children = child
-            logging.debug(parent)
+            if self.parent:
+                self.parent[-1].children.append(pk)
+            self.parent.append(pk)
+            self.decode_operator(packet)
+            child = pk.children
+            if child and self.part2:
+                pk.value = self.operations(pak_type, *child)
 
-            self.packets.append(parent)
-            return parent
+    @staticmethod
+    def operations(pak_type: int, *args):
+        if isinstance(args[0], Packet):
+            args = [i.value for i in args]
+
+        match pak_type:
+            case 0: result = sum(args)
+            case 1: result = math.prod(args)
+            case 2: result = min(args)
+            case 3: result = max(args)
+            case 5: result = 1 if args[0] > args[1] else 0
+            case 6: result = 1 if args[0] < args[1] else 0
+            case 7: result = 1 if args[0] == args[1] else 0
+        return result
 
     def decode_operator(self, packet: str):
         """ type 0:
@@ -69,20 +93,12 @@ class Decoder:
         14"""
         length_type = packet[6]
         if length_type == '0':
-            child = self.decode_packet(packet[7 + 15:])
+            self.decode_packet(packet[7 + 15:])
         else:
-            child = self.decode_packet(packet[7 + 11:])
-
-        if isinstance(child, list):
-            child = child.copy()
-            self.children.clear()
-        return child
+            self.decode_packet(packet[7 + 11:])
 
     @staticmethod
     def decode_literal(encoded: str) -> tuple[int, str | None]:
-        """ Represents a single number.
-        >>> print(Decoder(parsers.inline_test('D2FE28')).decode_packet('110100101111111000101000')[0])
-        2021"""
         chunks = deque([encoded[i:i + 5] for i in range(0, len(encoded), 5)])
         binary = ''
         while True:
@@ -113,10 +129,40 @@ class Decoder:
         31
         """
         self.decode_packet(self.encoded)
-        logging.debug(self.packets)
         return sum([i.version for i in self.packets])
 
+    def part_2(self):
+        """test part 2:
+        >>> print(Decoder(parsers.inline_test('C200B40A82')).part_2())
+        3
 
-# print(Decoder(parsers.lines(loader.get())).decode())  # 1002
-print(Decoder(parsers.inline_test('04005AC33890')).part_1())
+        >>> print(Decoder(parsers.inline_test('04005AC33890')).part_2())
+        54
+
+        >>> print(Decoder(parsers.inline_test('880086C3E88112')).part_2())
+        7
+
+        >>> print(Decoder(parsers.inline_test('CE00C43D881120')).part_2())
+        9
+
+        >>> print(Decoder(parsers.inline_test('D8005AC2A8F0')).part_2())
+        1
+
+        >>> print(Decoder(parsers.inline_test('F600BC2D8F')).part_2())
+        0
+
+        >>> print(Decoder(parsers.inline_test('9C005AC2F8F0')).part_2())
+        0
+
+        >>> print(Decoder(parsers.inline_test('9C0141080250320F1802104A08')).part_2())
+        1
+        """
+        self.part2 = True
+        self.decode_packet(self.encoded)
+        return self.root.value
+
+
+# print(Decoder(parsers.lines(loader.get())).part_1())  # 1002
+print(Decoder(parsers.lines(loader.get())).part_2())  #
+# print(Decoder(parsers.inline_test('D2318C6318C621')).part_1())
 
