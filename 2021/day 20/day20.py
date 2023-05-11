162c4def18cf6595513bec5049aeb2eef56a0aea
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-05-11 16:08:30 +0300

    compacting

--- 2021/day 20/day20.py
+++ 2021/day 20/day20.py
@@ -11,35 +11,31 @@ def parse(data: list[list[str]]) -> tuple[NDArray, NDArray]:
     return algorithm, image
 
 
-def enhance(algorithm: NDArray, image: NDArray, step: int) -> NDArray:
-    value = 1 if algorithm[0] == 1 and step % 2 == 0 else 0
-    image = np.pad(image, pad_width=2, mode='constant', constant_values=value)
-    output = np.zeros(shape=image.shape, dtype=np.dtype('u1'))
-
-    for index, val in np.ndenumerate(image[1:-1, 1:-1]):
-        row = index[0] + 1
-        col = index[1] + 1
-        adjacent = image[row - 1:row + 2, col - 1:col + 2]
-        flat = adjacent.ravel().astype(dtype=str)
-        f = ''.join(flat)
-        output[row][col] = algorithm[int(f, 2)]
-    return output[1:-1, 1:-1]
-
-
-def start(data: list, steps: int) -> int:
+def enhance(data, steps: int) -> int:
     """test part 1:
-    >>> print(start(parsers.blocks('test.txt'), 2))
+    >>> print(enhance(parsers.blocks('test.txt'), 2))
     35
 
-    test part 1:
-    >>> print(start(parsers.blocks('test.txt'), 50))
+    test part 2:
+    >>> print(enhance(parsers.blocks('test.txt'), 50))
     3351
     """
     algorithm, image = parse(data)
     for step in range(1, steps + 1):
-        image = enhance(algorithm, image, step)
+        value = 1 if algorithm[0] == 1 and step % 2 == 0 else 0
+        image = np.pad(image, pad_width=2, mode='constant', constant_values=value)
+        output = np.zeros(shape=image.shape, dtype=np.dtype('u1'))
+
+        for index in np.ndindex(image[1:-1, 1:-1].shape):
+            row = index[0] + 1
+            col = index[1] + 1
+            adjacent = image[row - 1:row + 2, col - 1:col + 2]
+            flat = adjacent.ravel().astype(dtype=str)
+            f = ''.join(flat)
+            output[row][col] = algorithm[int(f, 2)]
+        image = output[1:-1, 1:-1]
     return np.count_nonzero(image)
 
 
-print(start(parsers.blocks(loader.get()), 2))  # 5316
-print(start(parsers.blocks(loader.get()), 50))  # 16728
+print(enhance(parsers.blocks(loader.get()), 2))  # 5316
+print(enhance(parsers.blocks(loader.get()), 50))  # 16728
