78fec70b809cc6ff96a7fea77e47a7f988d9e94d
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-05-26 00:00:47 +0300

    day 04 part 2

--- 2020/day 04/day04.py
+++ 2020/day 04/day04.py
@@ -1,28 +1,38 @@
+import re
 from tools import parsers, loader
 
 
-def parse(data: list) -> list[dict]:
-    passports = []
-    for block in data:
-        strings = [val for row in block for val in row.split()]  # flatten list & split
-        passports.append({i: j for i, j in (s.split(':') for s in strings)})
-    return passports
+def part_1(data: list) -> int:
+    """
+    >>> print(part_1(parsers.blocks('test.txt')))
+    2"""
+    passports = [' '.join(i) for i in data]
+    out = 0
+    valid_full = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    for passport in passports:
+        if all(i in passport for i in valid_full):
+            out += 1
+    return out
 
 
-def validate(passport: dict) -> bool:
-    valid_full = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
-    keys = set(passport.keys())
-    diff = valid_full.difference(keys)
-    if not diff or diff == {'cid'}:
-        return True
-    return False
+def part_2(data: list) -> int:
+    """
+    >>> print(part_2(parsers.blocks('test.txt')))
+    2"""
+    passports = [' '.join(i) for i in data]
+    out = 0
+    for passport in passports:
+        patterns = re.compile(r"(byr:19[2-9]\d|byr:200[0-2])"
+                              r"|(iyr:201\d|iyr:2020)"
+                              r"|(eyr:202\d|eyr:2030)"
+                              r"|(hgt:1[5-8]\dcm|hgt:19[0-3]cm|hgt:59in|hgt:6\din|hgt:7[0-6]in)"
+                              r"|(hcl:#[0-9a-f]{6})"
+                              r"|(ecl:(amb|blu|brn|gry|grn|hzl|oth))"
+                              r"|(pid:\d{9}\b)")
+        result = list(re.finditer(patterns, passport))
+        out += 1 if len(result) == 7 else 0
+    return out
 
 
-def start(data: list) -> int:
-    passports = parse(data)
-    valid = [validate(passport) for passport in passports]
-    return sum(valid)
-
-
-print(start(parsers.blocks('test.txt')))
-print(start(parsers.blocks(loader.get())))  # 206
+print(part_1(parsers.blocks(loader.get())))  # 206
+print(part_2(parsers.blocks(loader.get())))  # 123
