35b002bb58d8578cb603ffdb41134dc8e9d3d9d4
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-05-29 22:44:25 +0300

    day 06

---
+++ 2020/day 06/day06.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from tools import parsers, loader
+
+
+def count(data):
+    """
+    >>> print(count(parsers.blocks('test.txt')))
+    (11, 6)"""
+    part_1 = 0
+    part_2 = 0
+    for block in data:
+        letters = [i for line in block for i in list(line)]
+        part_1 += len(set(letters))
+        people = len(block)
+        for i in Counter(letters).values():
+            part_2 += 1 if i == people else 0
+    return part_1, part_2
+
+
+print(count(parsers.blocks(loader.get())))  # 6662, 3382
