5d136bd38127cfbd1f0a978c207694000d99bbbf
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-06-27 12:49:42 +0300

    day 15 part 2
    brute force, slow

--- 2020/day 15/day15.py
+++ 2020/day 15/day15.py
@@ -1,49 +1,37 @@
-from dataclasses import dataclass, field
 from tools import parsers, loader
+from collections import deque
 
 
 TEST = "0,3,6"
 
 
-@dataclass
-class Number:
-    value: int
-    order: list = field(default_factory=list)
-
-    def __eq__(self, other: int):
-        return self.value == other
-
-    def is_first(self):
-        return len(self.order) <= 1
-
-    def get_new_number(self):
-        return self.order[-1] - self.order[-2]
-
-
-def recite(numbers: list):
+def recite(numbers: list, steps: int):
     """
-    >>> print(recite(parsers.inline_test(TEST)))
-    436"""
-    numbers = [int(i) for i in numbers[0].split(',')]
-    called_numbers = [Number(value=val, order=[i + 1]) for i, val in enumerate(numbers)]
+    >>> print(recite(parsers.inline_test(TEST), 2020))
+    436
 
-    previous_number = called_numbers[-1]
+    >>> print(recite(parsers.inline_test(TEST), 30000000))
+    175594"""
+    numbers = [int(i) for i in numbers[0].split(',')]
+    called_numbers = {val: deque([i + 1], maxlen=2) for i, val in enumerate(numbers)}
+
+    previous_number = called_numbers[numbers[-1]]
+    new_value = None
     step = len(numbers)
-    while step != 2020:
+    while step != steps:
         step += 1
-        if previous_number.is_first():
+        if len(previous_number) <= 1:
             new_value = 0
         else:
-            new_value = previous_number.get_new_number()
-        num = (x for x in called_numbers if x.value == new_value)
-        try:
-            n = next(num)
-        except StopIteration:
-            n = Number(new_value)
-            called_numbers.append(n)
-        n.order.append(step)
-        previous_number = n
-    return previous_number.value
+            new_value = previous_number[-1] - previous_number[-2]
+
+        if previous_number := called_numbers.get(new_value):
+            previous_number.append(step)
+        else:
+            previous_number = called_numbers[new_value] = deque([step], maxlen=2)
+
+    return new_value
 
 
-print(recite(parsers.lines(loader.get())))  # 763
+print(recite(parsers.lines(loader.get()), 2020))  # 763
+print(recite(parsers.lines(loader.get()), 30000000))  # 1876406
