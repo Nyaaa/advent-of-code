d437f8bfb75b5675b4f65b7d0cb9ee460d49b18e
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-07-16 22:02:15 +0300

    day 14 part 2

--- 2020/day 14/day14.py
+++ 2020/day 14/day14.py
@@ -1,3 +1,4 @@
+from itertools import product
 import re
 from dataclasses import dataclass, field
 from tools import parsers, loader
@@ -8,7 +9,7 @@ class Chunk:
     mask: str
     commands: list = field(default_factory=list)
 
-    def write(self):
+    def write(self) -> dict[int, int]:
         output = {}
         for addr, val in self.commands:
             bin_val = format(val, 'b').zfill(36)
@@ -19,6 +20,20 @@ class Chunk:
                     case '0': out += '0'
                     case _: out += bin_val[i]
             output[addr] = int(out, 2)
+        return output
+
+    def write_v2(self) -> dict[int, int]:
+        output = {}
+        for addr, val in self.commands:
+            bin_val = format(addr, 'b').zfill(36)
+            out = ''
+            for i, v in enumerate(self.mask):
+                match v:
+                    case '1': out += '1'
+                    case '0': out += bin_val[i]
+                    case 'X': out += 'X'
+            for a in (out.replace('X', '{}').format(*p) for p in product('01', repeat=out.count('X'))):
+                output[int(a, 2)] = val
         return output
 
 
@@ -32,14 +47,18 @@ class Bitmap:
                 addr, val = re.findall(r'\d+', line)
                 self.program[-1].commands.append((int(addr), int(val)))
 
-    def part_1(self):
+    def count(self, part2: bool) -> int:
         """
-        >>> print(Bitmap(parsers.lines('test.txt')).part_1())
-        165"""
+        >>> print(Bitmap(parsers.lines('test.txt')).count(part2=False))
+        165
+
+        >>> print(Bitmap(parsers.lines('test2.txt')).count(part2=True))
+        208"""
         result = {}
         for chunk in self.program:
-            result.update(chunk.write())
+            result.update(chunk.write() if not part2 else chunk.write_v2())
         return sum(result.values())
 
 
-print(Bitmap(parsers.lines(loader.get())).part_1())  # 5875750429995
+print(Bitmap(parsers.lines(loader.get())).count(part2=False))  # 5875750429995
+print(Bitmap(parsers.lines(loader.get())).count(part2=True))  # 5272149590143
