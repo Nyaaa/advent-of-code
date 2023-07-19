77f64e163f1f6fe764fc89b28a48c35b8d542187
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-07-19 12:02:37 +0300

    day 11 part 2

--- 2020/day 11/day11.py
+++ 2020/day 11/day11.py
@@ -25,5 +25,39 @@ class Lobby:
                 self.map = new_map
         return np.count_nonzero(self.map == 2)
 
+    def line_of_sight(self, index: tuple) -> int:
+        directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        occupied = 0
+        grid_size = self.map.shape[0] - 1
+        for direction in directions:
+            next_cell = (index[0] + direction[0], index[1] + direction[1])
+            while (0 <= next_cell[0] <= grid_size) and (0 <= next_cell[1] <= grid_size):
+                if self.map[next_cell] == 1:
+                    break
+                if self.map[next_cell] == 2:
+                    occupied += 1
+                    break
+                next_cell = (next_cell[0] + direction[0], next_cell[1] + direction[1])
+        return occupied
+
+    def part_2(self):
+        """
+        >>> print(Lobby(parsers.lines('test.txt')).part_2())
+        26"""
+        while True:
+            new_map = self.map.copy()
+            for index, value in np.ndenumerate(self.map):
+                visible_occupied = self.line_of_sight(index)
+                if value == 1 and visible_occupied == 0:
+                    new_map[index] = 2
+                elif value == 2 and visible_occupied >= 5:
+                    new_map[index] = 1
+            if np.array_equal(self.map, new_map):
+                break
+            else:
+                self.map = new_map
+        return np.count_nonzero(self.map == 2)
+
 
 print(Lobby(parsers.lines(loader.get())).part_1())  # 2470
+print(Lobby(parsers.lines(loader.get())).part_2())  # 2259
