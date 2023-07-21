aafc2588db2e1742ce579f855c363281be850d4d
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-07-21 20:35:41 +0300

    day 23 part 2

--- 2020/day 23/day23.py
+++ 2020/day 23/day23.py
@@ -1,48 +1,59 @@
-from collections import deque
-
 from tools import parsers, loader
-
 
 TEST = '389125467'
 
 
 class Cups:
-    def __init__(self, data: list):
-        self.circle = deque([int(i) for i in list(data[0])])
-        self.full_circle = self.circle.copy()
-        self.current_cup = self.circle[0]
+    def __init__(self, data: list[str], part2: bool = False):
+        data = [int(i) for i in data[0]]
+        if part2:
+            data.extend(list(range(10, 1_000_001)))
+        self.circle = {number: data[(i + 1) % len(data)] for i, number in enumerate(data)}
+        self.current_cup = data[0]
+        self.min, self.max = min(data), max(data)
 
     def get_destination(self, cups: list) -> int:
         destination = self.current_cup - 1
         while True:
-            if destination < min(self.full_circle):
-                destination = max(self.full_circle)
+            if destination < self.min:
+                destination = self.max
             if destination in cups:
                 destination -= 1
             else:
                 break
         return destination
 
-    def rotate(self, index: int, condition: int):
-        while self.circle[index] != condition:
-            self.circle.rotate(1)
+    def play(self, steps: int):
+        for _ in range(steps):
+            cups = [cup1 := self.circle[self.current_cup],
+                    cup2 := self.circle[cup1],
+                    self.circle[cup2]]
+            destination = self.get_destination(cups)
+            self.circle[self.current_cup] = self.circle[cups[-1]]
+            self.current_cup = self.circle[cups[-1]]
+            self.circle[cups[-1]] = self.circle[destination]
+            self.circle[destination] = cups[0]
 
-    def play(self) -> str:
+    def part_1(self):
         """"
-        >>> print(Cups(parsers.inline_test(TEST)).play())
+        >>> print(Cups(parsers.inline_test(TEST)).part_1())
         67384529"""
+        self.play(100)
+        result = []
+        step = self.circle[1]
+        for _ in self.circle:
+            result.append(step)
+            step = self.circle[step]
+        return ''.join(str(i) for i in result[:-1])
 
-        for _ in range(1, 101):
-            self.rotate(0, self.current_cup)
-            self.circle.rotate(-1)
-            cups = [self.circle.popleft() for _ in range(3)]
-            self.rotate(-1, self.get_destination(cups))
-            self.circle.extend(cups)
-            self.rotate(0, self.current_cup)
-            self.current_cup = self.circle[1]
-        self.rotate(0, 1)
-        self.circle.popleft()
-        return ''.join(str(i) for i in self.circle)
+    def part_2(self):
+        """"
+        >>> print(Cups(parsers.inline_test(TEST), True).part_2())
+        149245887792"""
+        self.play(10_000_000)
+        cup1 = self.circle[1]
+        return cup1 * self.circle[cup1]
 
 
-print(Cups(parsers.lines(loader.get())).play())  # 97342568
+print(Cups(parsers.lines(loader.get())).part_1())  # 97342568
+print(Cups(parsers.lines(loader.get()), True).part_2())  # 902208073192
