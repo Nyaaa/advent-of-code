f78e583996a5c24f9066a9e007bd8c0f8a1bdc37
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-07-26 14:01:18 +0300

    day 08

---
+++ 2019/day 08/day08.py
@@ -0,0 +1,42 @@
+from tools import parsers, loader
+import textwrap
+import numpy as np
+from numpy.typing import NDArray
+
+np.set_printoptions(linewidth=np.inf)
+
+
+class SIF:
+    def __init__(self, data: list, width: int, height: int):
+        self.layers: list[NDArray] = []
+        for layer in textwrap.wrap(data[0], width * height):
+            layer = np.asarray(list(layer), dtype=int)
+            layer = layer.reshape((height, width))
+            self.layers.append(layer)
+
+    def part_1(self):
+        zeroes = [np.count_nonzero(layer == 0) for layer in self.layers]
+        m = min(i for i in zeroes if i > 0)
+        layer = self.layers[zeroes.index(m)]
+        return np.count_nonzero(layer == 1) * np.count_nonzero(layer == 2)
+
+    def get_non_transparent(self, index):
+        for i in self.layers[1:]:
+            pixel = i[index]
+            if pixel != 2:
+                return pixel
+
+    def part_2(self):
+        image = self.layers[0]
+        while 2 in image:
+            for i, val in np.ndenumerate(image):
+                if val == 2:
+                    image[i] = self.get_non_transparent(i)
+        image = image.astype(str)
+        image[image == '1'] = '█'
+        image[image == '0'] = ' '
+        return image
+
+
+print(SIF(parsers.lines(loader.get()), 25, 6).part_1())  # 2032
+print(SIF(parsers.lines(loader.get()), 25, 6).part_2())  # CFCUG
