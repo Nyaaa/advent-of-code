480f62edd588f04f0285feb5337154fa998f6099
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-07-26 12:17:26 +0300

    day 06

---
+++ 2019/day 06/day06.py
@@ -0,0 +1,26 @@
+from tools import parsers, loader
+import networkx as nx
+
+
+class Orbits:
+    def __init__(self, data: list):
+        self.planets = nx.Graph()
+        for line in data:
+            planet1, planet2 = line.split(')')
+            self.planets.add_edge(planet1, planet2)
+
+    def part_1(self):
+        """
+        >>> print(Orbits(parsers.lines('test.txt')).part_1())
+        42"""
+        return sum(nx.dijkstra_path_length(self.planets, 'COM', p) for p in self.planets.nodes)
+
+    def part_2(self):
+        """
+        >>> print(Orbits(parsers.lines('test2.txt')).part_2())
+        4"""
+        return nx.dijkstra_path_length(self.planets, 'YOU', 'SAN') - 2
+
+
+print(Orbits(parsers.lines(loader.get())).part_1())  # 186597
+print(Orbits(parsers.lines(loader.get())).part_2())  # 412
