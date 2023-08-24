23455773e03bf6e5b383f7010b7b2f0e35670b50
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-08-24 11:43:11 +0300

    day 18 attempt #2, slow

--- 2019/day 18/day18.py
+++ 2019/day 18/day18.py
@@ -1,7 +1,17 @@
+from collections import deque
 from tools import parsers, loader, common
 import numpy as np
 import networkx as nx
 from string import ascii_uppercase, ascii_lowercase
+from typing import NamedTuple
+
+
+class State(NamedTuple):
+    location: tuple
+    keys: dict
+    steps: int
+    graph: nx.Graph
+    path: str
 
 
 class Maze:
@@ -21,37 +31,32 @@ class Maze:
         self.start = next(i for i, node in self.G.nodes(data=True) if node['label'] == '@')
         self.keys = [(i, l['label']) for i, l in self.G.nodes(data=True) if l['label'] in ascii_lowercase]
 
-    def find_path(self, G, current, t, keys):
-        steps = 0
-        while True:
-            if not t:
-                options = []
-                for target in keys.keys():
-                    n = nx.dijkstra_path_length(G, current, target)
-                    if n < float('inf'):
-                        options.append(target)
-                if len(options) > 1:
-                    r = {}
-                    for i in options:
-                        results = self.find_path(G.copy(), current, i, keys.copy())
-                        r[i] = results
-                    t = min(r, key=r.get)
-                else:
-                    t = options[0]
-            distance = nx.dijkstra_path_length(G, current, t)
-            steps += distance
-            current = t
-            keys.pop(current)
-            t = None
-            unlocked = G.nodes[current]['label'].upper()
-            try:
-                door = next(i for i, node in G.nodes(data=True) if node['label'] == unlocked)
-                G.add_edges_from(G.edges(door), weight=1)
-            except StopIteration:
-                if keys:
-                    continue
-                else:
-                    return steps
+    def find_path(self):
+        init_keys = {k: v for k, v in self.keys}
+        paths = deque([State(self.start, init_keys, 0, self.G, '')])
+        solution = float('inf')
+        while paths:
+            loc, keys, distance, G, path = paths.popleft()
+            # print(path)
+            if len(path) == len(self.keys):
+                if distance < solution:
+                    solution = distance
+                continue
+            if distance > solution:
+                continue
+            for key, letter in keys.items():
+                _distance = nx.dijkstra_path_length(G, loc, key)
+                if _distance != float('inf'):
+                    _G = G.copy()
+                    try:
+                        door = next(i for i, node in _G.nodes(data=True) if node['label'] == letter.upper())
+                        _G.add_edges_from(_G.edges(door), weight=1)
+                    except StopIteration:
+                        pass
+                    _keys = keys.copy()
+                    del _keys[key]
+                    paths.appendleft(State(key, _keys, distance + _distance, _G, path + letter))
+        return solution
 
     def part_1(self):
         """
@@ -59,11 +64,12 @@ class Maze:
         86
 
         >>> print(Maze(parsers.lines('test2.txt')).part_1())
-        132"""
-        current = self.start
-        keys = {k: v for k, v in self.keys}
-        steps = self.find_path(self.G, current, None, keys)
-        return steps
+        132
+
+        >>> print(Maze(parsers.lines('test4.txt')).part_1())
+        81"""
+        return self.find_path()
 
 
-print(Maze(parsers.lines('test4.txt')).part_1())
+print(Maze(parsers.lines('test3.txt')).part_1())
+# print(Maze(parsers.lines(loader.get())).part_1())
