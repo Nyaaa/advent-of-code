5db98c546429fec0f0714463d01c98199b96de4a
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-05 15:28:00 +0300

    day 04

---
+++ 2017/day 04/day04.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from itertools import combinations
+
+from tools import parsers, loader
+
+
+def part_1(data: list[str]) -> int:
+    result = 0
+    for line in data:
+        if Counter(line.split()).most_common(1)[0][1] == 1:
+            result += 1
+    return result
+
+
+def part_2(data: list[str]) -> int:
+    result = 0
+    for line in data:
+        words = line.split()
+        if Counter(words).most_common(1)[0][1] == 1 and \
+                all(Counter(a) != Counter(b) for a, b in combinations(words, 2)):
+            result += 1
+    return result
+
+
+print(part_1(parsers.lines(loader.get())))  # 325
+print(part_2(parsers.lines(loader.get())))  # 119
