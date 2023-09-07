9dbbae234240c1c7b3245a21001c746b1124be7b
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-07 09:36:53 +0300

    day 06

---
+++ 2017/day 06/day06.py
@@ -0,0 +1,33 @@
+from tools import parsers, loader
+
+
+def reallocate_mem(data: str, part2: bool) -> int:
+    """
+    >>> print(reallocate_mem('0\\t2\\t7\\t0', False))
+    5
+
+    >>> print(reallocate_mem('0\\t2\\t7\\t0', True))
+    4"""
+    data = [int(i) for i in data.split('\t')]
+    seen = set()
+    counter = 0
+    while True:
+        if s := tuple(data) in seen:
+            if not part2:
+                break
+            else:
+                seen, counter, part2 = {s}, 0, False
+        seen.add(tuple(data))
+        highest = max(data)
+        index = data.index(highest)
+        data[index] = 0
+        while highest > 0:
+            index = (index + 1) % len(data)
+            data[index] += 1
+            highest -= 1
+        counter += 1
+    return counter
+
+
+print(reallocate_mem(parsers.string(loader.get()), False))  # 6681
+print(reallocate_mem(parsers.string(loader.get()), True))  # 2392
