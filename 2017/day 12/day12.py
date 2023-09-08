9306850032422cc71647e180d3c20112951814a7
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-09 01:41:39 +0300

    day 12

---
+++ 2017/day 12/day12.py
@@ -0,0 +1,28 @@
+import re
+from tools import parsers, loader
+import networkx as nx
+
+
+class Pipes:
+    def __init__(self, data: list[str]):
+        self.G = nx.Graph()
+        for line in data:
+            name, *names = re.findall(r'\w+', line)
+            for i in names:
+                self.G.add_edge(name, i)
+
+    def part_1(self):
+        """
+        >>> print(Pipes(parsers.lines('test.txt')).part_1())
+        6"""
+        return len(nx.single_source_shortest_path(self.G, '0'))
+
+    def part_2(self):
+        """
+        >>> print(Pipes(parsers.lines('test.txt')).part_2())
+        2"""
+        return len(list(nx.connected_components(self.G)))
+
+
+print(Pipes(parsers.lines(loader.get())).part_1())  # 175
+print(Pipes(parsers.lines(loader.get())).part_2())  # 213
