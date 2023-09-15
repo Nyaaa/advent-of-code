1c004992c39a27789611fd275a20afe14e686059
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-15 17:04:01 +0300

    day 19

---
+++ 2017/day 19/day19.py
@@ -0,0 +1,27 @@
+from tools import parsers, loader
+
+
+def main(data: list[str]) -> tuple[str, int]:
+    """
+    >>> print(main(parsers.lines('test.txt', strip=False)))
+    ('ABCDEF', 38)"""
+    grid = dict()
+    for i, row in enumerate(data):
+        for j, cell in enumerate(row):
+            if cell != ' ' and cell != '\n':
+                grid[complex(i, j)] = cell
+    direction = 1-0j
+    location = min(grid, key=lambda x: x.real)
+    letters = ''
+    length = 0
+    while value := grid.get(location):
+        length += 1
+        if value.isalpha():
+            letters += value
+        elif value == '+':
+            direction *= 1j if location + direction * 1j in grid else -1j
+        location += direction
+    return letters, length
+
+
+print(main(parsers.lines(loader.get(), strip=False)))  # ('GPALMJSOY', 16204)
