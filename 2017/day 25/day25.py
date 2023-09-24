278d814bddf7e2bb14584301b26d9e8fa2f0c431
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-25 00:09:08 +0300

    day 25

---
+++ 2017/day 25/day25.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from tools import parsers, loader
+import re
+
+
+def start(data: list[list[str]]) -> int:
+    """
+    >>> print(start(parsers.blocks('test.txt')))
+    3"""
+    state = re.findall(r' ([A-Z])\.', data[0][0])[0]
+    steps = int(re.findall(r'\d+', data[0][1])[0])
+    tape = defaultdict(int)
+    instructions = dict()
+    for i in data[1:]:
+        values = [line.split()[-1][:-1] for line in i]
+        instructions[(values[0], 0)] = values[2:5]
+        instructions[(values[0], 1)] = values[6:]
+    position = 0
+    for i in range(steps):
+        value = tape[position]
+        operation = instructions.get((state, value))
+        tape[position] = int(operation[0])
+        position += 1 if operation[1] == 'right' else -1
+        state = operation[2]
+    return sum(tape.values())
+
+
+print(start(parsers.blocks(loader.get())))  # 2870
