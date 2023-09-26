ed31e3da33c99c08cc03d30d6f9d4b0fc2c3cb34
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-26 14:28:34 +0300

    day 05

---
+++ 2016/day 05/day05.py
@@ -0,0 +1,29 @@
+from tools import parsers, loader
+from itertools import count
+from hashlib import md5
+
+
+def start(data: str) -> tuple[str, str]:
+    """
+    >>> print(start('abc'))
+    ('18f47a30', '05ace8e3')"""
+    part1 = ''
+    part2 = dict()
+    for i in count():
+        string = f'{data}{i}'
+        _hash = md5(bytes(string, 'utf-8')).hexdigest()
+        if _hash.startswith('00000'):
+            pos = _hash[5]
+            part1 += pos
+            try:
+                pos = int(pos)
+            except ValueError:
+                continue
+            if 0 <= pos <= 7 and not part2.get(pos):
+                part2[pos] = _hash[6]
+        if len(part2) == 8:
+            break
+    return part1[:8], ''.join(v for k, v in sorted(part2.items()))
+
+
+print(start(parsers.string(loader.get())))  # c6697b55, 8c35d1ab
