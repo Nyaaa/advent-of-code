ec44afc3119c7a6d10872b05e73e6d246e2728ce
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-26 16:12:40 +0300

    day 06

---
+++ 2016/day 06/day06.py
@@ -0,0 +1,19 @@
+from collections import Counter
+import numpy as np
+from tools import parsers, loader
+
+
+def start(data: list[str]) -> tuple[str, str]:
+    """
+    >>> print(start(parsers.lines('test.txt')))
+    ('easter', 'advent')"""
+    arr = np.asarray([list(line) for line in data])
+    part1 = part2 = ''
+    for i in range(arr.shape[1]):
+        counts = Counter(arr[:, i]).most_common()
+        part1 += counts[0][0]
+        part2 += counts[-1][0]
+    return part1, part2
+
+
+print(start(parsers.lines(loader.get())))  # kjxfwkdh, xrwcsnps
