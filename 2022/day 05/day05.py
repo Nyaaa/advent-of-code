b88dc39b6565a56dc4f335030c35c6df77982a10
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-09-29 23:01:05 +0300

    day 05 refactoring

--- 2022/day 05/day05.py
+++ 2022/day 05/day05.py
@@ -1,63 +1,42 @@
-# [B]                     [N]     [H]
-# [V]         [P] [T]     [V]     [P]
-# [W]     [C] [T] [S]     [H]     [N]
-# [T]     [J] [Z] [M] [N] [F]     [L]
-# [Q]     [W] [N] [J] [T] [Q] [R] [B]
-# [N] [B] [Q] [R] [V] [F] [D] [F] [M]
-# [H] [W] [S] [J] [P] [W] [L] [P] [S]
-# [D] [D] [T] [F] [G] [B] [B] [H] [Z]
-#  1   2   3   4   5   6   7   8   9
-
 import queue
+import re
+from collections.abc import Generator
 from tools import parsers, loader
-init_load = ['DHNQTWVB', 'DWB', 'TSQWJC', 'FJRNZTP', 'GPVJMST', 'BWFTN', 'BLDQFHVN', 'HPFR', 'ZSMBLNPH']
-test_load = ['ZN', 'MCD', 'P']
-test = ['move 1 from 2 to 1', 'move 3 from 1 to 3', 'move 2 from 2 to 1', 'move 1 from 1 to 2']
 
 
 class Crane:
-    def __init__(self, data, load):
-        self.data = data
+    def __init__(self, data: list[list[str]]) -> None:
+        self.data = data[1]
         self.stack_list = []
-        self.size = len(load)
+        load = [''.join(s) for s in zip(*data[0][:-1])]
+        stacks = list(filter(None, (re.findall(r'\w+', i.strip()) for i in load)))
+        self.size = len(stacks)
         for i in range(self.size):
             self.stack_list.append(queue.LifoQueue())
-            for crate in load[i]:
+            for crate in stacks[i][0][::-1]:
                 self.stack_list[i].put(crate)
 
-    def get_moves(self):
+    def get_moves(self) -> Generator[tuple[int, int, int]]:
         for move in self.data:
             move = move.split(' ')
             yield int(move[1]), int(move[3]), int(move[5])
 
-    def part_1(self):
-        """test part 1:
-        >>> print(Crane(test, test_load).part_1())
-        CMZ"""
-        for amount, _from, _to in self.get_moves():
-            for _ in range(amount):
-                crate = self.stack_list[_from - 1].get()
-                self.stack_list[_to - 1].put(crate)
-        return self.result()
+    def start(self, part2: bool) -> str:
+        """
+        >>> print(Crane(parsers.blocks('test.txt')).start(False))
+        CMZ
 
-    def part_2(self):
-        """test part 1:
-        >>> print(Crane(test, test_load).part_2())
+        >>> print(Crane(parsers.blocks('test.txt')).start(True))
         MCD"""
         for amount, _from, _to in self.get_moves():
-            crates = []
-            for _ in range(amount):
-                crates.insert(0, self.stack_list[_from - 1].get())
+            if not part2:
+                crates = [self.stack_list[_from - 1].get() for _ in range(amount)]
+            else:
+                crates = reversed([self.stack_list[_from - 1].get() for _ in range(amount)])
             for crate in crates:
                 self.stack_list[_to - 1].put(crate)
-        return self.result()
-
-    def result(self):
-        message = ""
-        for stack in self.stack_list:
-            message += stack.get()
-        return message
+        return ''.join(stack.get() for stack in self.stack_list)
 
 
-print(Crane(parsers.lines(loader.get()), init_load).part_1())  # PSNRGBTFT
-print(Crane(parsers.lines(loader.get()), init_load).part_2())  # BNTZFPMMW
+print(Crane(parsers.blocks(loader.get())).start(part2=False))  # PSNRGBTFT
+print(Crane(parsers.blocks(loader.get())).start(part2=True))  # BNTZFPMMW
