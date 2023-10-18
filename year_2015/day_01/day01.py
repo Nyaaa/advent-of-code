9233213fc2fe6704ff1dbef5ac60348138588963
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-10-18 22:50:18 +0300

    itertools ftw

--- year_2015/day_01/day01.py
+++ year_2015/day_01/day01.py
@@ -1,18 +1,8 @@
+from itertools import accumulate, dropwhile
+
 from tools import loader, parsers
 
+data = [1 if val == '(' else -1 for val in parsers.string(loader.get())]
 
-def part_1(data: str) -> int:
-    return data.count('(') - data.count(')')
-
-
-def part_2(data: str) -> int:
-    floor = 0
-    for i, val in enumerate(data):
-        floor += 1 if val == '(' else -1
-        if floor < 0:
-            return i + 1
-    return 0
-
-
-print(part_1(parsers.string(loader.get())))  # 232
-print(part_2(parsers.string(loader.get())))  # 1783
+print(sum(data))  # 232
+print(next(dropwhile(lambda x: x[1] >= 0, enumerate(accumulate(data), start=1)))[0])  # 1783
