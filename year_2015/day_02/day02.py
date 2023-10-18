3606e9d22b953acfb98975b925184ff643c3e788
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-10-19 00:27:14 +0300

    day 02

---
+++ year_2015/day_02/day02.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+from math import prod
+
+from tools import loader, parsers
+
+data = parsers.lines(loader.get())
+
+print(sum(2 * sum(sides) + min(sides) for sides in
+          (list(map(prod, combinations(map(int, line.split('x')), 2)))
+           for line in data)))  # 1606483
+print(sum(2 * (j[0] + j[1]) + prod(j) for j in
+          (sorted(map(int, i.split('x')))for i in data)))  # 3842356
