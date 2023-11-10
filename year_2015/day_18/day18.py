e58cdee12b9af57c6be5a1bbb764f20f7724c679
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-11-10 10:38:06 +0300

    add numba

--- year_2015/day_18/day18.py
+++ year_2015/day_18/day18.py
@@ -1,37 +1,36 @@
 import numpy as np
+from numba import njit
 
 from tools import common, loader, parsers
 
 
-def animation(data: list[str], steps: int, part2: bool) -> int:
+@njit
+def animation(data: tuple[str], steps: int, part2: bool) -> int:
     """
-    >>> print(animation(parsers.lines('test.txt'), steps=4, part2=False))
+    >>> print(animation(tuple(parsers.lines('test.txt')), steps=4, part2=False))
     4
-    >>> print(animation(parsers.lines('test.txt'), steps=5, part2=True))
+    >>> print(animation(tuple(parsers.lines('test.txt')), steps=5, part2=True))
     17"""
     arr = np.array([[1 if i == '#' else 0 for i in row] for row in data],
                    dtype=np.dtype('u1'))
-    corners = np.ix_((0, -1), (0, -1))
+    m, n = arr.shape
     if part2:
-        arr[corners] = 1
+        arr[::m - 1, ::n - 1] = 1
     step = 0
     while step != steps:
         arr_copy = np.zeros_like(arr)
         for i, val in np.ndenumerate(arr):
-            adj = sum(j for i, j in common.get_adjacent(arr, i, with_corners=True))
-            match val, adj:
-                case 1, 2 | 3:
-                    arr_copy[i] = 1
-                case 0, 3:
-                    arr_copy[i] = 1
-                case _:
-                    arr_copy[i] = 0
+            adj = sum([j for i, j in common.get_adjacent(arr, i, with_corners=True)])
+            if (val == 1 and adj in (2, 3)) or (val == 0 and adj == 3):
+                arr_copy[i] = 1
+            else:
+                arr_copy[i] = 0
         arr = arr_copy
         if part2:
-            arr[corners] = 1
+            arr[::m - 1, ::n - 1] = 1
         step += 1
     return np.count_nonzero(arr)
 
 
-print(animation(parsers.lines(loader.get()), steps=100, part2=False))  # 821
-print(animation(parsers.lines(loader.get()), steps=100, part2=True))  # 886
+print(animation(tuple(parsers.lines(loader.get())), steps=100, part2=False))  # 821
+print(animation(tuple(parsers.lines(loader.get())), steps=100, part2=True))  # 886
