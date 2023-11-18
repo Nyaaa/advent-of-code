c1bcc3b383eee77439cbf2bf31a4eab29be0b08c
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-11-18 11:22:44 +0300

    day 19

---
+++ year_2015/day_19/day19.py
@@ -0,0 +1,25 @@
+import re
+
+from tools import loader, parsers
+
+DATA = parsers.blocks(loader.get())
+
+molecule = DATA[1][0][::-1]
+replacements = dict(line[::-1].split(' >= ') for line in DATA[0])
+
+
+part1 = set()
+for value, key in replacements.items():
+    for match in re.finditer(key, molecule):
+        part1.add(f'{molecule[:match.start()]}{value}{molecule[match.end():]}')
+
+print(len(part1))  # 576
+
+
+part2 = 0
+subs = re.compile('|'.join(replacements))
+while molecule != 'e':
+    molecule = re.sub(subs, lambda x: replacements[x.group()], molecule, count=1)
+    part2 += 1
+
+print(part2)  # 207
