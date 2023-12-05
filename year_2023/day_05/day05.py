020860e56e4e773530086946d793b79577b28303
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-12-05 13:29:41 +0300

    day 05 part 2

--- year_2023/day_05/day05.py
+++ year_2023/day_05/day05.py
@@ -1,38 +1,61 @@
+import logging
 import re
+from itertools import count
 
-from tools import loader, parsers
-
-
-def planting(data: list[list[str]]) -> int:
-    """
-    >>> print(planting(parsers.blocks('test.txt')))
-    35"""
-    seeds = list(map(int, re.findall(r'\d+', data[0][0])))
-    tables = []
-    for block in data[1:]:
-        _block = []
-        for line in block[1:]:
-            nums = tuple(map(int, line.split()))
-            destination = range(nums[0], nums[0] + nums[2])
-            source = range(nums[1], nums[1] + nums[2])
-            _block.append((source, destination))
-        tables.append(_block)
-        pass
-    results = []
-    for seed in seeds:
-        current = seed
-        for table in tables:
-            match = None
-            for line in table:
-                if current in line[0]:
-                    diff = current - line[0].start
-                    match = line[1].start + diff
-                    pass
-            current = match or current
-            pass
-        results.append(current)
-    return min(results)
+from tools import loader, parsers, timer
 
 
-print(planting(parsers.blocks(loader.get())))  # 484023871
+class Plants:
+    def __init__(self, data: list[list[str]]) -> None:
+        self.seeds = list(map(int, re.findall(r'\d+', data[0][0])))
+        self.tables = []
+        for block in data[1:]:
+            _block = []
+            for line in block[1:]:
+                nums = tuple(map(int, line.split()))
+                destination = range(nums[0], nums[0] + nums[2])
+                source = range(nums[1], nums[1] + nums[2])
+                _block.append((source, destination))
+            self.tables.append(_block)
 
+    def part_1(self) -> int:
+        """
+        >>> print(Plants(parsers.blocks('test.txt')).part_1())
+        35"""
+        result = float('inf')
+        for seed in self.seeds:
+            current = seed
+            for table in self.tables:
+                match = None
+                for line in table:
+                    if current in line[0]:
+                        match = line[1].start + current - line[0].start
+                current = match or current
+            result = min(result, current)
+        return result
+
+    @timer.wrapper
+    def part_2(self) -> int:
+        """Bruteforce, takes a while.
+        >>> print(Plants(parsers.blocks('test.txt')).part_2())
+        46"""
+        a = list(zip(self.seeds[::2], self.seeds[1::2]))
+        seeds = [range(i[0], i[0] + i[1]) for i in a]
+
+        for seed in count(1):
+            if seed % 1_000_000 == 0:
+                logging.debug(seed)
+            current = seed
+            for table in self.tables[::-1]:
+                match = None
+                for line in table:
+                    if current in line[1]:
+                        match = line[0].start + current - line[1].start
+                current = match or current
+            if any(current in s for s in seeds):
+                return seed
+        raise ValueError('Solution not found')
+
+
+print(Plants(parsers.blocks(loader.get())).part_1())  # 484023871
+print(Plants(parsers.blocks(loader.get())).part_2())  # 46294175
