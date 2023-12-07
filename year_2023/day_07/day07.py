195f3064a4a5e34b7080b0c480ed9cb9ae925fbe
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2023-12-07 12:10:14 +0300

    day 07 part 2

--- year_2023/day_07/day07.py
+++ year_2023/day_07/day07.py
@@ -25,19 +25,21 @@ class Hand:
     hand_type: CardTypes = field(init=False)
 
     def __post_init__(self) -> None:
-        counts = list(Counter(self.cards).values())
-        if 5 in counts:
+        counts = Counter(self.cards).most_common()
+        if 1 in self.cards:
+            jokers = self.cards.count(1)
+            if jokers != 5:
+                counts.remove((1, jokers))
+                counts[0] = (counts[0][0], counts[0][1] + jokers)
+
+        if counts[0][1] == 5:
             self.hand_type = CardTypes.five_kind
-        elif 4 in counts:
+        elif counts[0][1] == 4:
             self.hand_type = CardTypes.four_kind
-        elif 3 in counts and 2 in counts:
-            self.hand_type = CardTypes.full_house
-        elif 3 in counts:
-            self.hand_type = CardTypes.three_kind
-        elif counts.count(2) == 2:
-            self.hand_type = CardTypes.two_pair
-        elif 2 in counts:
-            self.hand_type = CardTypes.one_pair
+        elif counts[0][1] == 3:
+            self.hand_type = CardTypes.full_house if counts[1][1] == 2 else CardTypes.three_kind
+        elif counts[0][1] == 2:
+            self.hand_type = CardTypes.two_pair if counts[1][1] == 2 else CardTypes.one_pair
         else:
             self.hand_type = CardTypes.high_card
 
@@ -48,13 +50,19 @@ class Hand:
         raise ValueError('Hands are identical')
 
 
-def camel_cards(data: list[str]) -> int:
+
+def camel_cards(data: list[str], part2: bool) -> int:
     """
-    >>> print(camel_cards(parsers.lines('test.txt')))
-    6440"""
+    >>> print(camel_cards(parsers.lines('test.txt'), part2=False))
+    6440
+    >>> print(camel_cards(parsers.lines('test.txt'), part2=True))
+    5905"""
     card_values = {v: k for k, v in enumerate('23456789TJQKA', start=2)}
+    if part2:
+        card_values['J'] = 1
     hands = [Hand(cards=[card_values[i] for i in k], bid=int(v))
              for k, v in (x.split() for x in data)]
+
     hand_rank = 1
     for card_type in CardTypes:
         queue = deque([h for h in hands if h.hand_type == card_type])
@@ -64,15 +72,13 @@ def camel_cards(data: list[str]) -> int:
                 left.rank = hand_rank
                 hand_rank += 1
                 break
-            is_smallest = all(left < i for i in queue)
-            if not is_smallest:
+            if not all(left < i for i in queue):
                 queue.append(left)
             else:
                 left.rank = hand_rank
                 hand_rank += 1
-            pass
     return sum(i.rank * i.bid for i in hands)
 
 
-# print(camel_cards(parsers.lines('test.txt')))
-print(camel_cards(parsers.lines(loader.get())))  # 249483956
+print(camel_cards(parsers.lines(loader.get()), part2=False))  # 249483956
+print(camel_cards(parsers.lines(loader.get()), part2=True))  # 252137472
