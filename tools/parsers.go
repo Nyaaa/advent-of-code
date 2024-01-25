3df20c0c75a8de926641977c8502c50f2463a67e
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-01-25 17:27:50 +0300

    go parser

---
+++ tools/parsers.go
@@ -0,0 +1,29 @@
+package tools
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+func Read_lines(input string) []string {
+    b, err := os.ReadFile(input)
+    if err != nil {
+        log.Fatal(err)
+    }
+	return strings.Split(string(b), "\n")
+}
+
+func Split_blocks(input []string) [][]string {
+    var output [][]string
+    var temp []string
+    for _, item := range input {
+        if item == "" {
+            output = append(output, temp)
+            temp = nil
+        } else {
+            temp = append(temp, item)
+        }
+    }
+    return output
+}
