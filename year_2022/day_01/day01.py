d08c6dfe266cfcaee67d2b74a6e49a793728fbe1
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-01-29 16:40:02 +0300

    golfing

--- year_2022/day_01/day01.py
+++ year_2022/day_01/day01.py
@@ -1,23 +1,13 @@
 from tools import loader, parsers
 
 
-class Puzzle:
-    def __init__(self, data: list[list[str]]) -> None:
-        self.sums = [sum(int(i) for i in block) for block in data]
-
-    def part_1(self) -> int:
-        """test part 1:
-        >>> print(Puzzle(parsers.blocks('test.txt')).part_1())
-        24000"""
-        return max(self.sums)
-
-    def part_2(self) -> int:
-        """test part 2:
-        >>> print(Puzzle(parsers.blocks('test.txt')).part_2())
-        45000"""
-        self.sums.sort(reverse=True)
-        return sum(self.sums[0:3])
+def puzzle(data: list[list[str]]) -> tuple[int, int]:
+    """
+    >>> puzzle(parsers.blocks('test.txt'))
+    (24000, 45000)
+    """
+    sums = sorted(sum(int(i) for i in block) for block in data)
+    return sums[-1], sum(sums[-3:])
 
 
-print(Puzzle(parsers.blocks(loader.get())).part_1())  # 69883
-print(Puzzle(parsers.blocks(loader.get())).part_2())  # 207576
+print(puzzle(parsers.blocks(loader.get())))  # 69883, 207576
--- year_2022/day_02/day02.py
+++ year_2022/day_02/day02.py
@@ -3,37 +3,32 @@ from tools import loader, parsers
 moves = {'A': 1, 'X': 1,  # rock
          'B': 2, 'Y': 2,  # paper
          'C': 3, 'Z': 3}  # scissors
-wins = [(3, 1), (1, 2), (2, 3)]
-res = {'X': 'loss', 'Y': 'draw', 'Z': 'win'}
+wins = {(3, 1), (1, 2), (2, 3)}
+res = {'X': 0, 'Y': 3, 'Z': 6}
 test = ['A Y', 'B X', 'C Z']
+reverse_wins = {1: {0: 3, 3: 1, 6: 2}, 2: {0: 1, 3: 2, 6: 3}, 3: {0: 2, 3: 3, 6: 1}}
 
 
-def check_win(i: int, j: int) -> tuple[str, int]:
+def check_win(i: int, j: int) -> tuple[int, int]:
     if i == j:
-        return 'draw', j + 3
+        return 3, j
     if (i, j) in wins:
-        return 'win', j + 6
-    return 'loss', j + 0
+        return 6, j
+    return 0, j
 
 
 def part_1(data: list[str]) -> int:
     """test part 1:
     >>> print(part_1(test))
     15"""
-    return sum(check_win(moves[r[0]], moves[r[2]])[1] for r in data)
+    return sum(sum(check_win(moves[r[0]], moves[r[2]])) for r in data)
 
 
 def part_2(data: list[str]) -> int:
     """test part 2:
     >>> print(part_2(test))
     12"""
-    part2 = 0
-    for r in data:
-        i = moves[r[0]]
-        for n in 1, 2, 3:
-            if check_win(i, n)[0] == res[r[2]]:
-                part2 += check_win(i, n)[1]
-    return part2
+    return sum(reverse_wins[moves[r[0]]][res[r[2]]] + res[r[2]] for r in data)
 
 
 print(part_1(parsers.lines(loader.get())))  # 11603
