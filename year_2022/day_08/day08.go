7aad9a72d1fc2a74282fd3be8a4021839a336177
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-02-16 10:38:06 +0300

    day 08

---
+++ year_2022/day_08/day08.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"aoc/tools"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func solve(input string) (int, int) {
+	forest := [][]int{}
+	data := tools.ReadLines(input)
+	part1 := 0
+	part2 := 0
+
+	for _, line := range data {
+		if line == "" {
+			continue
+		}
+
+		line := strings.Split(line, "")
+		forest = append(forest, tools.StrToInt(line))
+	}
+
+	for row := 0; row < len(forest); row++ {
+		for col := 0; col < len(forest); col++ {
+			visible, score := evaluateTile(row, col, forest)
+			part2 = max(part2, score)
+			part1 += visible
+		}
+	}
+
+	return part1, part2
+}
+
+func evaluateTile(row int, col int, forest [][]int) (int, int) {
+	left := forest[row][:col]
+	left = append(left[:0:0], left...)
+	right := forest[row][col+1:]
+	top := []int{}
+	bottom := []int{}
+
+	for i := 0; i < len(forest); i++ {
+		tile := forest[i][col]
+		if i < row {
+			top = append(top, tile)
+		} else if i > row {
+			bottom = append(bottom, tile)
+		}
+	}
+
+	slices.Reverse(top)
+	slices.Reverse(left)
+
+	return getScore([][]int{left, right, top, bottom}, forest[row][col])
+}
+
+func getScore(vision [][]int, tile int) (int, int) {
+	isVisible := 0
+	score := 1
+
+	for _, line := range vision {
+		canSee := 0
+		clear := true
+
+		for _, tree := range line {
+			canSee++
+
+			if tree >= tile {
+				clear = false
+
+				break
+			}
+		}
+
+		score *= canSee
+
+		if clear {
+			isVisible = 1
+		}
+	}
+
+	return isVisible, score
+}
+
+func main() {
+	fmt.Println(solve("test.txt"))
+	fmt.Println(solve(tools.GetData(2022, 8)))
+}
--- year_2022/day_08/day08.py
+++ year_2022/day_08/day08.py
@@ -1,90 +1,37 @@
-from collections import Counter
-from itertools import chain
+from pathlib import Path
 
-from tools import loader, parsers
+import numpy as np
 
-test = ['30373', '25512', '65332', '33549', '35390']
-d = parsers.lines(loader.get())
+from tools import loader
 
 
-class Forest:
-    def __init__(self, data: list[str]) -> None:
-        self.grid = [[int(i) for i in row] for row in data]
-        self.x = len(self.grid[0])
-        self.y = len(self.grid)
-        self.tree = None
+def forest(data: str | Path) -> tuple[int, int]:
+    """
+    >>> print(forest('test.txt'))
+    (21, 8)"""
+    arr = np.genfromtxt(data, delimiter=1, dtype=int)
+    part1 = part2 = 0
 
-    def get_score(self, side: list[int], direction: int) -> int:
-        """part 2"""
+    for loc, val in np.ndenumerate(arr):
+        left = arr[loc[0], :loc[1]][::-1]
+        right = arr[loc[0], loc[1]+1:]
+        top = arr[:loc[0], loc[1]][::-1]
+        bottom = arr[loc[0]+1:, loc[1]]
 
-        _score = 0
-        if direction == 1:
-            side = side[::-1]  # iterating backwards for left & up
+        part1 += (np.all(val > left) or np.all(val > right) or
+                  np.all(val > top) or np.all(val > bottom))
 
-        for i in side:
-            if i < self.tree:
-                _score += 1
-            else:
-                _score += 1
-                break
+        score = 1
+        for line in left, right, top, bottom:
+            count = 0
+            for tree in line:
+                count += 1
+                if tree >= val:
+                    break
+            score *= count
+        part2 = max(part2, score)
 
-        return _score
-
-    def is_visible(self, _x: int, _y: int) -> tuple[str, int]:
-        """part 1"""
-
-        # getting edges
-        if _x in (0, self.x - 1) or _y in (0, self.y - 1):
-            return 'Visible', 0
-
-        # getting rows
-        left = self.grid[_y][:_x]
-        right = self.grid[_y][_x + 1:]
-
-        # getting columns
-        up = [self.grid[i][_x] for i in range(_y)]
-        down = [self.grid[i][_x] for i in range(_y + 1, self.y)]
-
-        s_right = self.get_score(right, 0)
-        s_left = self.get_score(left, 1)
-        s_up = self.get_score(up, 1)
-        s_down = self.get_score(down, 0)
-        _score = s_right * s_left * s_up * s_down
-
-        if (self.tree > max(left)
-                or self.tree > max(right)
-                or self.tree > max(up)
-                or self.tree > max(down)):
-            return 'Visible', _score
-
-        return 'Hidden', _score
-
-    def solve(self, part: int) -> int:
-        """test part 1:
-        >>> print(Forest(test).solve(1))
-        21
-
-        test part 2:
-        >>> print(Forest(test).solve(2))
-        8"""
-
-        vis = []
-        scores = []
-        for _x in range(self.x):
-            vis_row = []
-            score_row = []
-            for _y in range(self.y):
-                self.tree = self.grid[_x][_y]
-                visible, score = self.is_visible(_y, _x)
-                vis_row.append(visible)
-                score_row.append(score)
-            vis.append(vis_row)
-            scores.append(score_row)
-
-        if part == 1:
-            return Counter(list(chain(*vis)))['Visible']
-        return max(list(chain(*scores)))
+    return part1, part2
 
 
-print(Forest(d).solve(1))  # 1543
-print(Forest(d).solve(2))  # 595080
+print(forest(loader.get()))  # 1543, 595080
