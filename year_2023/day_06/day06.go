ae40684716d7bf147a1f53714d111354d156992f
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-03-14 14:41:12 +0300

    day 06

---
+++ year_2023/day_06/day06.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"aoc/tools"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func solve(input string, part2 bool) int {
+	data := tools.ReadLines(input)
+	times := strings.Fields(data[0])[1:]
+	distances := strings.Fields(data[1])[1:]
+	wins := 1
+
+	if part2 {
+		times = []string{strings.Join(times, "")}
+		distances = []string{strings.Join(distances, "")}
+	}
+
+	for i := 0; i < len(times); i++ {
+		time, _ := strconv.Atoi(times[i])
+		distance, _ := strconv.Atoi(distances[i])
+		w := 0
+
+		for i := 1; i <= time+1; i++ {
+			if distance < (time-i)*i {
+				w++
+			}
+		}
+
+		wins *= w
+	}
+
+	return wins
+}
+
+func main() {
+	data := tools.GetData(2023, 6)
+
+	fmt.Println(solve("test.txt", false), solve("test.txt", true))
+	fmt.Println(solve(data, false), solve(data, true))
+}
--- year_2023/day_06/day06.py
+++ year_2023/day_06/day06.py
@@ -4,16 +4,12 @@ from more_itertools import transpose
 
 from tools import loader, parsers
 
-TEST = """Time:      7  15   30
-Distance:  9  40  200
-"""
-
 
 def race(data: list[str], part2: bool = False) -> int:
     """
-    >>> print(race(parsers.inline_test(TEST), part2=False))
+    >>> print(race(parsers.lines('test.txt'), part2=False))
     288
-    >>> print(race(parsers.inline_test(TEST), part2=True))
+    >>> print(race(parsers.lines('test.txt'), part2=True))
     71503"""
     records = list(transpose(list(map(int, re.findall(r'\d+', i.split(':')[1]))) for i in data))
     if part2:
