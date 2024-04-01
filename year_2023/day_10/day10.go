d9ba75cdd611885cc6dae26218d90e234d8090fe
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-04-01 14:21:03 +0300

    day 10 part 1

---
+++ year_2023/day_10/day10.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Nyaaa/advent-of-code/tools"
+	"strings"
+)
+
+type cart struct {
+	location, start, direction complex128
+}
+
+func (c *cart) rotate(tile string) {
+	switch tile {
+	case "L", "7":
+		if real(c.direction) == 0 {
+			c.direction *= -1i
+		} else {
+			c.direction *= 1i
+		}
+	case "J", "F":
+		if real(c.direction) == 0 {
+			c.direction *= 1i
+		} else {
+			c.direction *= -1i
+		}
+	}
+}
+
+func getTrack(c cart, pipes map[complex128]string) []complex128 {
+	track := []complex128{}
+
+	for {
+		track = append(track, c.location)
+		c.location += c.direction
+		c.rotate(pipes[c.location])
+
+		if c.location == c.start {
+			break
+		}
+	}
+
+	return track
+}
+
+func solve(input string) (int, int) {
+	data := tools.ReadLines(input)
+	part2 := 0
+	pipes := map[complex128]string{}
+	pipeChars := [7]string{"-", "|", "F", "7", "J", "L", "S"}
+	cart := cart{0, 0, 0}
+
+	for i, line := range data {
+		for j, tile := range strings.Split(line, "") {
+			location := complex(float64(i), float64(j))
+
+			for _, char := range pipeChars {
+				if char == tile {
+					pipes[location] = tile
+
+					if tile == "S" {
+						cart.location = location
+						cart.start = location
+					}
+				}
+			}
+
+			pipes[location] = tile
+		}
+	}
+
+	for _, i := range [4]complex128{1, -1, 1i, -1i} {
+		// This is potentially fragile if there are adjacent pipes that don't connect to S.
+		_, ok := pipes[cart.location+i]
+
+		if ok {
+			cart.direction = i
+
+			break
+		}
+	}
+
+	track := getTrack(cart, pipes)
+
+	return len(track) / 2, part2
+}
+
+func main() {
+	fmt.Println(solve("test.txt"))
+	fmt.Println(solve(tools.GetData(2023, 10)))
+}
