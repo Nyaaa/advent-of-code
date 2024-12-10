7dd6fa978ec7f947d73e4650674b37c15eb8a011
Nyaaa <1457047+Nyaaa@users.noreply.github.com>
2024-12-11 00:47:38 +0300

    2024 day 09 rewrite with Numba

--- year_2024/day_09/day09.py
+++ year_2024/day_09/day09.py
@@ -1,97 +1,69 @@
-from collections import deque
-from collections.abc import Iterable
+import numpy as np
+from numba import njit
+from numpy.typing import NDArray
 
 from tools import loader, parsers
 
 TEST = '2333133121414131402'
 
 
-def prep_data(data: str) -> deque[int]:
-    disk = deque([])
-    counter = 0
-    for i, val in enumerate(data):
-        if i % 2 == 0:
-            disk.extend(counter for _ in range(int(val)))
-            counter += 1
-        else:
-            disk.extend(-1 for _ in range(int(val)))
+def defrag_part1(disk: NDArray) -> NDArray:
+    right_ind = len(disk)
+    free_space = 0
+    while right_ind > free_space:
+        right_ind -= 1
+        right = disk[right_ind]
+        if right == -1:
+            continue
+        disk[right_ind] = -1
+        free_space = np.flatnonzero(disk == -1)[:1]
+        disk[free_space] = right
     return disk
 
 
-def get_result(done: Iterable[int]) -> int:
+@njit
+def defrag_part2(disk: NDArray) -> NDArray:
+    right = disk[-1]
+    while right != 0:
+        right_file = np.flatnonzero(disk == right)
+        right_ind = right_file[0]
+        all_free = np.flatnonzero(disk[:right_ind] == -1)
+        free_chunks = np.split(all_free, np.flatnonzero(np.diff(all_free) != 1) + 1)
+        good_chunk = [i for i in free_chunks if len(i) >= right_file.size]
+        if good_chunk:
+            free_space = good_chunk[0][0]
+            disk[right_ind: right_ind + right_file.size] = -1
+            disk[free_space:free_space + right_file.size] = right
+        right -= 1
+    return disk
+
+
+def defragment(data: str, part2: bool) -> int:
+    """
+    >>> print(defragment(TEST, part2=False))
+    1928
+    >>> print(defragment(TEST, part2=True))
+    2858"""
+    data_ = []
+    counter = 0
+    for i, val in enumerate(data):
+        if i % 2 == 0:
+            data_.extend(counter for _ in range(int(val)))
+            counter += 1
+        else:
+            data_.extend(-1 for _ in range(int(val)))
+
+    disk = np.asarray(data_, dtype=int)
+    fn = defrag_part2 if part2 else defrag_part1
+    disk = fn(disk)
+
     result = 0
-    for i, val in enumerate(done):
+    for i, val in enumerate(disk):
         if val == -1:
             continue
-        result += i * int(val)
+        result += i * val
     return result
 
 
-def part1(data: str) -> int:
-    """
-    >>> print(part1(TEST))
-    1928"""
-    done = []
-    disk = prep_data(data)
-
-    while disk:
-        while disk:
-            left = disk.popleft()
-            if left != -1:
-                done.append(left)
-            else:
-                break
-
-        if not disk:
-            break
-
-        right = disk.pop()
-        disk.appendleft(right)
-
-    return get_result(done)
-
-
-def part2(data: str) -> int:
-    """
-    >>> print(part2(TEST))
-    2858"""
-    disk = list(prep_data(data))
-
-    right = None
-    right_ind = None
-    while right_ind != 0:
-        if not right:
-            right = disk[-1]
-            right_ind = disk.index(right)
-        right_len = disk.count(right)
-
-        indices = []
-        left_ind = disk.index(-1)
-        for i, val in enumerate(disk[left_ind:right_ind], start=left_ind):
-            if val == -1:
-                indices.append(i)
-            else:
-                indices = []
-                continue
-
-            if len(indices) >= right_len and indices[0] < right_ind:
-                for i_right in range(right_len):
-                    disk[right_ind + i_right] = -1
-                for index in indices:
-                    disk[index] = right
-                right -= 1
-                right_ind = disk.index(right)
-                break
-        else:
-            last_moved = right
-            for val in reversed(disk[:right_ind]):
-                if val < last_moved and val != -1:
-                    right = val
-                    right_ind = disk.index(right)
-                    break
-
-    return get_result(disk)
-
-
-print(part1(parsers.string(loader.get())))  # 6340197768906
-print(part2(parsers.string(loader.get())))  # 6363913128533
+print(defragment(parsers.string(loader.get()), part2=False))  # 6340197768906
+print(defragment(parsers.string(loader.get()), part2=True))  # 6363913128533
